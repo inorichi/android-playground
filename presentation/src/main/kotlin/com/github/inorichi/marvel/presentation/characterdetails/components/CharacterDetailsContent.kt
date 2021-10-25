89b53e56f77ede4adc50f71ad2dd4a0668c29f83
inorichi <len@kanade.eu>
2021-10-25 21:01:42 +0200

    Improve details page (again)

--- presentation/src/main/kotlin/com/github/inorichi/marvel/presentation/characterdetails/components/CharacterDetailsContent.kt
+++ presentation/src/main/kotlin/com/github/inorichi/marvel/presentation/characterdetails/components/CharacterDetailsContent.kt
@@ -4,15 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import com.github.inorichi.marvel.domain.character.entity.CharacterDetails
 import com.github.inorichi.marvel.presentation.R
@@ -23,60 +26,90 @@ fun CharacterDetailsContent(
   modifier: Modifier = Modifier
 ) {
   LazyColumn(
-    modifier = modifier.fillMaxSize().padding(16.dp),
-    verticalArrangement = Arrangement.spacedBy(16.dp)
+    modifier = modifier
+      .fillMaxSize()
+      .padding(16.dp)
   ) {
     item {
-      Image(
-        painter = rememberImagePainter(character.thumbnail),
-        contentDescription = character.name,
-        modifier = Modifier
-          .width(200.dp)
-          .aspectRatio(3 / 4f)
-      )
+      Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+      ) {
+        Image(
+          painter = rememberImagePainter(character.thumbnail),
+          contentDescription = character.name,
+          modifier = Modifier
+            .width(200.dp)
+            .aspectRatio(3 / 4f)
+        )
+      }
     }
     if (character.description.isNotBlank()) {
       item {
-        Text(text = buildAnnotatedString {
-          withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
-            append(stringResource(R.string.character_description) + "\n")
-          }
-          append(character.description)
-        })
-      }
-    }
-    val wikiUrl = character.wikiUrl
-    if (!wikiUrl.isNullOrBlank()) {
-      item {
-        Text(text = buildAnnotatedString {
-          withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
-            append(stringResource(R.string.character_wiki_url) + "\n")
-          }
-          append(wikiUrl)
-        })
+        Column(
+          modifier = Modifier.padding(top = 16.dp),
+          verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+          CharacterDetailsHeader(
+            title = stringResource(R.string.character_description),
+            imageVector = ImageVector.vectorResource(R.drawable.ic_baseline_label_24)
+          )
+          Text(character.description, modifier = Modifier.padding(start = 40.dp))
+        }
       }
     }
     if (character.series.isNotEmpty()) {
       item {
-        Text(
-          text = stringResource(R.string.character_series),
-          fontWeight = FontWeight.Bold
+        CharacterDetailsHeader(
+          title = stringResource(R.string.character_series),
+          imageVector = ImageVector.vectorResource(R.drawable.ic_baseline_movie_24),
+          modifier = Modifier.padding(top = 16.dp)
         )
       }
       items(character.series) { series ->
-        Text(series.name)
+        Text(series.name, modifier = Modifier.padding(start = 40.dp, top = 8.dp))
       }
     }
     if (character.comics.isNotEmpty()) {
       item {
-        Text(
-          text = stringResource(R.string.character_comics),
-          fontWeight = FontWeight.Bold
+        CharacterDetailsHeader(
+          title = stringResource(R.string.character_comics).uppercase(),
+          imageVector = ImageVector.vectorResource(R.drawable.ic_baseline_chrome_reader_mode_24),
+          modifier = Modifier.padding(top = 16.dp)
         )
       }
       items(character.comics) { comic ->
-        Text(comic.name)
+        Text(comic.name, modifier = Modifier.padding(start = 40.dp, top = 8.dp))
       }
     }
+  }
+}
+
+@Composable
+private fun CharacterDetailsHeader(
+  title: String,
+  imageVector: ImageVector,
+  modifier: Modifier = Modifier
+) {
+  Column(
+    modifier = modifier,
+    verticalArrangement = Arrangement.spacedBy(8.dp)
+  ) {
+    Row(
+      horizontalArrangement = Arrangement.spacedBy(16.dp),
+      verticalAlignment = Alignment.CenterVertically
+    ) {
+      Icon(
+        imageVector = imageVector,
+        contentDescription = title
+      )
+      Text(
+        text = title.uppercase(),
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        letterSpacing = 1.25.sp
+      )
+    }
+    Divider(startIndent = 40.dp)
   }
 }
